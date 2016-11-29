9d671adc429d739eb9cb61e79c4cdcaecfd53f07
ArturMinnullin <minnullin.artur@gmail.com>
2016-11-29 15:28:15 +0300

    Style messages for telegram

--- lib/mix/tasks/send_messages.ex
+++ lib/mix/tasks/send_messages.ex
@@ -17,32 +17,37 @@ defmodule Mix.Tasks.SendMessages do
   end
 
   defp get_groups(user) do
-    time = Timex.to_unix Timex.shift(Timex.now, hours: 3)
+    time = Timex.to_unix Timex.now
 
     for group <- user.active_groups do
       response = HTTPotion.get "https://api.vk.com/method/wall.get",
-        query: %{owner_id: -String.to_integer(group), access_token: user.access_token}
+        query: %{owner_id: -String.to_integer(group), access_token: user.access_token, extended: 1}
 
       list = response.body
         |> Poison.decode!
         |> Map.get("response")
-      [count | objects] = list
+      group = List.first(list["groups"])
+      [count | objects] = list["wall"]
 
       if is_nil(user.last_checked_at) do
         text = List.first(objects)["text"]
 
         HTTPotion.get "https://api.telegram.org/bot#{System.get_env("TELEGRAM_KEY")}/sendMessage",
-              query: %{chat_id: user.telegram_chat_id, text: text}
+              query: %{chat_id: user.telegram_chat_id, text: format_message(group, text), parse_mode: "Markdown"}
       else
         for object <- objects do
           if object["date"] > user.last_checked_at do
             HTTPotion.get "https://api.telegram.org/bot#{System.get_env("TELEGRAM_KEY")}/sendMessage",
-              query: %{chat_id: user.telegram_chat_id, text: object["text"]}
+              query: %{chat_id: user.telegram_chat_id, text: format_message(group, object["text"]), parse_mode: "Markdown"}
           end
         end
       end
     end
 
     user |> Ecto.Changeset.change(%{last_checked_at: time}) |> VkBots.Repo.update
+  end
+
+  defp format_message(group, text) do
+    "[#{group["name"]}](vk.com/#{group["screen_name"]}): #{String.replace(text, "<br>", "\n")}"
   end
 end
