b67b16e9ec15d3c650634a0d9bd6d4d5f837ad43
ArturMinnullin <minnullin.artur@gmail.com>
2016-12-04 14:16:10 +0300

    Fix login again

--- web/controllers/bot_login_controller.ex
+++ web/controllers/bot_login_controller.ex
@@ -4,16 +4,18 @@ defmodule VkBots.BotLoginController do
   alias VkBots.User
 
   def request(conn, params) do
-    text = String.split(params["message"]["text"], " ")
+    unless is_nil(params["message"]["text"]) do
+      text = String.split(params["message"]["text"], " ")
 
-    if List.first(text) == "/start" do
-      user = Repo.get_by(User, uid: List.last(text))
-      unless is_nil(user) do
-        changeset = User.changeset(user, %{telegram_chat_id: params["message"]["chat"]["id"]})
-        Repo.update(changeset)
+      if List.first(text) == "/start" do
+        user = Repo.get_by(User, uid: List.last(text))
+        unless is_nil(user) do
+          changeset = User.changeset(user, %{telegram_chat_id: params["message"]["chat"]["id"]})
+          Repo.update(changeset)
 
-        HTTPotion.get "https://api.telegram.org/bot#{System.get_env("TELEGRAM_KEY")}/sendMessage",
-          query: %{chat_id: params["message"]["chat"]["id"], text: "Привет! Скоро сообщения начнут приходить :)", parse_mode: "HTML"}
+          HTTPotion.get "https://api.telegram.org/bot#{System.get_env("TELEGRAM_KEY")}/sendMessage",
+            query: %{chat_id: params["message"]["chat"]["id"], text: "Привет! Скоро сообщения начнут приходить :)", parse_mode: "HTML"}
+        end
       end
     end
 
